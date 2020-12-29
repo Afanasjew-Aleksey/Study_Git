3badb297a2dc9401fade697e161df76533a3c0ba
Aleksey <afanasjew.aleksey2020@gmail.com>
2020-12-29 22:36:39 +0400

    3 commit

--- src/com/company/Class1.java
+++ src/com/company/Class1.java
@@ -2,6 +2,13 @@ package com.company;
 
 public class Class1 {
     public static void XZ1(){
-        System.out.print("XZ1");
+        int a=5;
+        int b=10;
+        b--;
+        int[] mas=new int[5];
+        for (int i=0; i<b; i++){
+            mas[i]=5;
+        }
+        System.out.print(a/b);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("FF");
         int a=3;
         System.out.println("GG");
-
+    //FSfesg
         Class1.XZ1();
     }
 }
