0d388f8a76a81477e1e4f78b311b812c6b8e783a
Afanasjew-Aleksey <61011916+Afanasjew-Aleksey@users.noreply.github.com>
2020-12-29 21:59:52 +0400

    Create Class2.java

---
+++ src/com/company/Class2.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Class2 {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("FF");
+        int a=3;
+        System.out.println("GG");
+
+        Class2.XZ1();
+    }
+}
